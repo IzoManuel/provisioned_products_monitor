68602ee91c7b4db3b52a8b69f6e481a2a4c4ca06
Ubuntu <ubuntu@ip-10-0-1-117.ap-south-1.compute.internal>
2024-03-15 19:09:12 +0000

    Add users table to dashboard

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,5 +1,5 @@
 // Function to show details for selected row
-function showDetailsForRow(index) {
+function showProductDetailsForRow(index) {
     // Get the selected product using its index
     console.log('Openning')
     let selectedProduct = stale_products[index];
@@ -10,6 +10,27 @@ function showDetailsForRow(index) {
     detailsDiv.innerHTML = `<p><strong class="text-gray-600">Product Name:</strong> ${selectedProduct.Name}</p>
                              <p><strong class="text-gray-600 text-red-500">Duration:</strong> ${selectedProduct.duration}</p>
                              <p><strong class="text-gray-600">Status:</strong> ${selectedProduct.Status}</p>`;
+    selectedItemDetailsRow.style.display = 'block';
+}
+
+// Listen for radio button changes
+document.querySelectorAll('.radio').forEach(radio => {
+    radio.addEventListener('change', function() {
+        let selectedIndex = parseInt(this.id.replace('radio', ''));
+        showDetailsForRow(selectedIndex);
+    });
+});
+
+// Function to show details for selected row
+function showUserDetailsForRow(index) {
+    // Get the selected product using its index
+    console.log('Openning'+index)
+    let user = users[index];
+    console.log(user)
+    // Display details in the details section
+    let detailsDiv = document.getElementById('selectedItemDetails');
+    detailsDiv.innerHTML = `<p><strong class="text-gray-600">User email:</strong> ${user.email}</p>
+                             <p><strong class="text-gray-600">Number of provisioned products:</strong> ${user.product_count}</p>`;
     selectedItemDetailsRow.style.display = 'block';
 }
 
