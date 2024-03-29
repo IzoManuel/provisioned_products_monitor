2aee0464d10b56f61c2adbd9a4691b93532dee3a
IzoManuel <israel7manuel@gmail.com>
2024-03-29 08:57:02 +0300

    Remove .env and config.py from repository

--- config.py
+++
@@ -1,8 +0,0 @@
-# Threshold duration (in days) for identifying stale provisioned products
-stale_product_threshold_hours = 8
-
-# Threshold count for identifying users with a high number of provisioned products
-high_product_count_threshold = 2
-
-bucket_name = 'dg-cohort-01'
-file_key = 'team_israel_user_info3.json'
